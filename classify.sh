1965674fb0eb763f288204e97f273276ffeab24e
Matt Patenaude <matt@mattpatenaude.com>
2013-04-15 01:36:05 -0400

    Modified shell scripts to include searchio build warning

--- classify.sh
+++ classify.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 # This example assumes that you code in Python
+if [ ! -f searchio.so ];
+then
+	echo
+	echo "*** WARNING: PLEASE MAKE FIRST ***"
+	echo "Our project includes a C module that must be compiled."
+	echo "Run \"make\" before trying to use this script."
+	echo
+	exit
+fi
 
 # Main program to be executed
 MAIN="classify.py"
